40f57420fa4dc1da83461a62fda38013b3d989cb
ITO Tetsunosuke <tetsunosuke.ito@gmail.com>
2026-06-22 11:08:46 +0900

    feat: add hint page and configure SEO metadata including sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://xinniku-xxxkotsu-ramen.pages.dev',
   publicDir: 'public',
   outDir: 'dist',
   devToolbar: {
